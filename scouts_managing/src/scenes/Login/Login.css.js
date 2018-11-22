dd9716df0d38505fc1a9d3e679c86b6faa951a6e
Tiago Horta <tiago_horta95@hotmail.com>
2018-11-22 01:12:55 +0000

    SCTS_01 - Add initial login form

---
+++ scouts_managing/src/scenes/Login/Login.css.js
@@ -0,0 +1,41 @@
+const styles = {
+  body: { paddingLeft: 39, paddingTop: 24 },
+  button: {
+    height: 30,
+    marginLeft: 175,
+    marginTop: 24,
+    width: 52
+  },
+  header: {
+    alignItems: "center",
+    borderBottom: "1px solid black",
+    display: "flex",
+    height: 100,
+    paddingLeft: 39
+  },
+  headerTitle: {
+    fontFamily: "Tahoma",
+    fontSize: 28
+  },
+  input: {
+    width: 220
+  },
+  loginForm: {
+    border: "1px solid black",
+    borderRadius: 4,
+    height: 300,
+    width: 300
+  },
+  mainContainer: {
+    alignItems: "center",
+    display: "flex",
+    height: "100%",
+    justifyContent: "center",
+    width: "100%"
+  },
+  password: {
+    paddingTop: 24
+  }
+};
+
+export default styles;
---
+++ scouts_managing/src/scenes/Login/Login.js
@@ -0,0 +1,62 @@
+// React
+import React, { Component } from "react";
+// Styles
+import styles from "./Login.css";
+
+// Uma class é um componente que tem um estado
+// Uma const é um componente sem estado
+class Login extends Component {
+  // Estado do componente
+  constructor(props) {
+    super(props);
+    // Propriedades do estado
+    this.state = { email: "", password: "", loginSuccessful: false };
+  }
+
+  // Função que recebe o argumento event
+  handleEmailChange = event => {
+    const value = event.target.value;
+    // Metodo para atualizar o estado
+    this.setState({ email: value });
+  };
+  handlePasswordChange = event => {
+    const value = event.target.value;
+    this.setState({ password: value });
+  };
+  handleSubmit = () => {
+    const { email, password } = this.state;
+    if (email === "aep197@escoteiros.pt" && password === "escoteiros") {
+      this.setState({ loginSuccessful: true });
+    }
+  };
+  render() {
+    return (
+      <div style={styles.mainContainer}>
+        <div style={styles.loginForm}>
+          <div style={styles.header}>
+            <div style={styles.headerTitle}>AEP 197 QUELFES</div>
+          </div>
+          <div style={styles.body}>
+            <div>Email</div>
+            <input style={styles.input} onChange={this.handleEmailChange} />
+            <div style={styles.password}>Password</div>
+            <input
+              style={styles.input}
+              type="password"
+              onChange={this.handlePasswordChange}
+            />
+            <button
+              style={styles.button}
+              type="submit"
+              onClick={this.handleSubmit}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
